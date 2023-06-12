1c529d7715018617c68f9503eba3385e6d7d05fe
唯人 佐藤 <220079@jc-21.jp>
2023-06-12 15:18:15 +0900

    少し改善

--- MyGameEngine/Texture.cpp
+++ MyGameEngine/Texture.cpp
@@ -1,13 +1,14 @@
 #include "Texture.h"
-#include"Direct3D.h"
-#include<DirectXTex.h>
+#include "Direct3D.h"
+
+#include <DirectXTex.h>
 
 #pragma comment(lib,"DirectXTex.lib")
+
 
 Texture::Texture()
 	:pSampler_(nullptr), pSRV_(nullptr)
 {
-
 }
 
 Texture::~Texture()
@@ -15,23 +16,24 @@ Texture::~Texture()
 	Release();
 }
 
-HRESULT Texture::Load(std::string fileName)
+HRESULT Texture::Load(string filename)
 {
 	using namespace DirectX;
+	//////////画像読み込み部分（変更）
 
 	wchar_t wtext[FILENAME_MAX];
 	size_t ret;
-	mbstowcs_s(&ret, wtext, fileName.c_str(), fileName.length());
+	mbstowcs_s(&ret, wtext, filename.c_str(), filename.length());
+
 	TexMetadata metadata;
 	ScratchImage image;
 	HRESULT hr;
 	hr = LoadFromWICFile(wtext, WIC_FLAGS::WIC_FLAGS_NONE, &metadata, image);
+	/////////
 	if (FAILED(hr))
 	{
-		MessageBox(nullptr, "画像の読み込みに失敗しました", "エラー", MB_OK);
 		return E_FAIL;
 	}
-
 	//サンプラーの作成
 	D3D11_SAMPLER_DESC  SamDesc;
 	ZeroMemory(&SamDesc, sizeof(D3D11_SAMPLER_DESC));
@@ -40,28 +42,26 @@ HRESULT Texture::Load(std::string fileName)
 	SamDesc.AddressV = D3D11_TEXTURE_ADDRESS_CLAMP;
 	SamDesc.AddressW = D3D11_TEXTURE_ADDRESS_CLAMP;
 	hr = Direct3D::pDevice_->CreateSamplerState(&SamDesc, &pSampler_);
-	
 	if (FAILED(hr))
 	{
-		MessageBox(nullptr, "サンプラーの作成に失敗しました", "エラー", MB_OK);
 		return E_FAIL;
 	}
 
-	//シェーダ―リソースビューの作成
+	//シェーダーリソースビュー
 	D3D11_SHADER_RESOURCE_VIEW_DESC srv = {};
 	srv.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
 	srv.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
 	srv.Texture2D.MipLevels = 1;
+
 	hr = CreateShaderResourceView(Direct3D::pDevice_,
 		image.GetImages(), image.GetImageCount(), metadata, &pSRV_);
 	if (FAILED(hr))
-		
 	{
-		MessageBox(nullptr, "シェーダ―リソースビューの作成に失敗しました", "エラー", MB_OK);
-		return E_FAIL;
+		return S_FALSE;
 	}
-	return S_OK;
 
+
+	return S_OK;
 }
 
 void Texture::Release()
@@ -69,6 +69,3 @@ void Texture::Release()
 	SAFE_RELEASE(pSampler_);
 	SAFE_RELEASE(pSRV_);
 }
-
-
-
--- MyGameEngine/Texture.h
+++ MyGameEngine/Texture.h
@@ -1,8 +1,10 @@
 #pragma once
 #include <d3d11.h>
-#include "string"
-#include<wrl.h>
+#include <string>
+#include <wrl.h>
+
 using namespace Microsoft::WRL;
+using std::string;
 
 class Texture
 {
@@ -11,10 +13,8 @@ class Texture
 public:
 	Texture();
 	~Texture();
-	HRESULT Load(std::string fileName);
+	HRESULT Load(string filename);
 	void Release();
-
 	ID3D11SamplerState* GetSampler() { return pSampler_; }
 	ID3D11ShaderResourceView* GetSRV() { return pSRV_; }
 };
-
