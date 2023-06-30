2a454fb36686b7d227ad940b9c23e5f65067f25a
唯人 佐藤 <220079@jc-21.jp>
2023-06-30 10:26:12 +0900

    一応できた！

--- MyGameEngine/Simple3D.hlsl
+++ MyGameEngine/Simple3D.hlsl
@@ -12,6 +12,8 @@ cbuffer global
 {
 	float4x4	matWVP;			// ワールド・ビュー・プロジェクションの合成行列
 	float4x4	matNormal;           // ワールド行列
+	float4		diffuseColor;		// ディフューズカラー（マテリアルの色）
+	bool		isTexture;		// テクスチャ貼ってあるかどうか
 };
 
 //───────────────────────────────────────
@@ -53,7 +55,19 @@ float4 PS(VS_OUT inData) : SV_Target
 {
 	float4 lightSource = float4(1.0, 1.0, 1.0, 1.0);
 	float4 ambentSource = float4(0.2, 0.2, 0.2, 1.0);
-	float4 diffuse = lightSource * g_texture.Sample(g_sampler, inData.uv) * inData.color;
-	float4 ambient = lightSource * g_texture.Sample(g_sampler, inData.uv) * ambentSource;
-	return (diffuse + ambient);
+	float4 diffuse;
+	float4 ambient;
+	if (isTexture == false)
+	{
+		diffuse = lightSource * diffuseColor * inData.color;
+		ambient = lightSource * diffuseColor * ambentSource;
+	}
+	else
+	{
+		 diffuse = lightSource * g_texture.Sample(g_sampler, inData.uv) * inData.color;
+		 ambient = lightSource * g_texture.Sample(g_sampler, inData.uv) * ambentSource;
+		
+	}
+	return diffuse + ambient;
+
 }
